a6806c135334dd6d04e14827b6612318749d5ea1
= <=>
2022-09-24 21:27:47 +0530

    updated

--- printf.c
+++ printf.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main()
 {
-	int x=2, y=2;
-	printf("%i", x+y);
+	int x=2, y=5;
+	int sum;
+	sum=x+y;
+	printf("%i",sum);
 	return 0;
 }
---
+++ scanf.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	int x;
+	float num;
+
+	
+	printf("enter any char\n");
+	scanf("%c",&c);
+	printf("%c\n",c);	
+
+	printf ("\nenter any no\n");
+	scanf("%i",&x);
+	printf("%i",x);
+	
+	printf("\nenter any float val\n");
+	scanf("%f", &num);
+	printf("%f",num);
+	
+
+	return 0;
+}
